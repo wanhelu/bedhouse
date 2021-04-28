12456a9bfe79bf9ef4e9baf184884f7fc4da0477
youwish-paker <2064934008@qq.com>
2021-04-28 10:12:56 +0800

    first

---
+++ bedhouse-server/src/main/java/com/example/bedhouseserver/BedhouseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bedhouseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedhouseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedhouseServerApplication.class, args);
+    }
+
+}
---
+++ bedhouse-server/src/test/java/com/example/bedhouseserver/BedhouseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bedhouseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedhouseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
